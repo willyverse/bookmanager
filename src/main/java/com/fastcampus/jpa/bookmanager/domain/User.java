ae5d3c567cdf57f5a26473324a71868fa5e5d99c
Chanyoung Kim <whqnd1106@gmail.com>
2022-03-15 00:24:03 +0900

    connect h2 database add JpaRepository (extends CrudRepository)

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -3,15 +3,30 @@ package com.fastcampus.jpa.bookmanager.domain;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+@Entity // Key 값이 반드시 필요함
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String email;
+
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 }
